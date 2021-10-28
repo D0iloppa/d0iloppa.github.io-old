7101bda9cf0b5d2614674195d4abbf1bac9afb46
d0iloppa <kdi3939@gmail.com>
2021-10-28 17:18:42 +0900

    test

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -47,12 +47,20 @@ module.exports = {
         activity: '교내 정보보안 동아리 "INVISIBLE SHIELD" 활동',
       },
       {
+        date: '2011.04 ~ 2011.08',
+        activity: '교내 정보전산원 서버관리부 근로장학생',
+      },
+      {
         date: '2015.08 ~ 2015.11',
         activity: '한국전력공사 대학생 서포터즈 활동',
       },
       {
         date: '2020.12 ~ 2021.07',
         activity: '직업전문학교 자바응용sw(앱) 개발자 양성과정 교육 수료',
+      },
+      {
+        date: '2021.08 ~ ',
+        activity: '(주)웰컨 개발 연구원',
       },
     ],
 
@@ -75,10 +83,28 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
+        title: '교내 소프트웨어공학 팀 프로젝트',
+        description: '풋살,농구 등 생활체육에 즉흥적 참여를 위해 GPS 기반으로 팀 매칭시켜주는 애플리케이션',
+        techStack: ['android'],
+      },
+      {
         title: 'KEEP WALK',
         description: '음식의 칼로리만 서버 DB에 저장해 놓고, 여러 키워드들에 대하여 (ex. 햄버거, 롯데리아, 맥도날드…) 검색이 가능하도록 키워드를 배열로 관리한다. 이는 자동으로 사용자의 신체정보에 맞춰 활동해야 할 거리를 계산해준다. 이 거리정보를 기준으로 거리만큼 떨어져 있는 해당 음식점을 매핑시켜 준다.',
-        techStack: ['android', ''],
+        techStack: ['android'],
         thumbnailUrl: 'profile/keepwalk.png',
+      },
+      {
+        title: '쇼핑몰 홈페이지 제작 및 하이버 연계 상품 등록 모듈 제작',
+        description: '쇼핑몰 사이트 제작, 상품관리, 사진보정, 소셜네트워크 마케팅 관리(페이스북 연동 상품 광고 및 카탈로그 관리) , 하이버 연계 상품 등록 모듈 제작',
+        techStack: ['html','css','js','python','photoshop CS6'],
+        thumbnailUrl: 'profile/kkl.png',
+        links: {
+          post: '',
+          github: '',
+          googlePlay: '',
+          appStore: '',
+          demo: 'https://kwonkoolook.com/',
+        },
       },
 
 
---
+++ src/components/certificates-section/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import SectionHeader from '../section-header';
+import IconButtonBar from '../icon-button-bar';
+
+import './style.scss';
+
+const CertSection = ({ certificates }) => {
+  if (!certificates || timescertificatestamps.length < 2) return null;
+  return (
+    <div className="certificates-section-wrapper">
+      <div className="certificates-section">
+        <SectionHeader title="certificates" />
+        <div className="body">
+          {certificates.map((certificates, index) =>
+            index === 0 ? null : (
+              <div className="certificates" key={index}>
+                <div className="date">{certificates.date}</div>
+                <div className="title">{certificates.title}</div>
+                <div className="description">{certificates.description}</div>
+              </div>
+            ),
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CertSection;
