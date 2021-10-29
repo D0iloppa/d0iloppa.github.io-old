c68ac1272b12c56ae8cf0c0d37654191a711a262
d0iloppa <kdi3939@gmail.com>
2021-10-29 10:07:37 +0900

    bboomi up

--- src/components/tmi-section/index.js
+++ src/components/tmi-section/index.js
@@ -9,15 +9,33 @@ const TMISection = ({}) => {
     <div className="tmi-section-wrapper">
     <div className="tmi-section">
       <div className="body">
-          <p>한낱 일기장으로 치부할 수도 있을</p>
-          <p>나의 흔적, 나의 자취들이</p>
-          <p>어느 한 사람의 마음 한 켠에서라도</p>
-          <p>흐뭇해지기를 소망하는 바입니다.</p>
-          
-          
-          권도일이라고 하는 사람이올시다
+          <div className="front">
 
-          See a [my Naver Blog](http://kdi3939.blog.me/) hosted on Naver.
+            <p>위대해지고 싶은 것</p>
+            <p>그것은 누구나</p>
+            <p>마음 속 간직한 욕망이 아닐까 싶습니다.</p>
+
+            <p>한낱 일기장으로 치부할 수도 있을</p>
+            <p>나의 흔적, 나의 자취들이</p>
+            <p>어느 한 사람의 마음 한 켠에서라도</p>
+            <p>흐뭇해지기를 소망하는 바입니다.</p>
+          </div>
+
+          <div className="bbomi">
+            <div className="bbomi-gal">
+              <img src="/assets/bbomi/pm03.jpg"/>
+              <img src="/assets/bbomi/pm02.jpg"/>
+              <img src="/assets/bbomi/pm01.jpg"/>
+              <img src="/assets/bbomi/pm04.jpg"/>
+            </div>
+            <p> BBomi's moment of my Precious </p>   
+          </div>
+          
+          <div className="back">
+            <p>권도일이라고 하는 사람이올시다</p>
+            <p>See a <a href="https://blog.naver.com/kdi3939">my Naver Blog</a> hosted on Naver.</p>
+          </div>
+          
       </div>
     </div>
   </div>
